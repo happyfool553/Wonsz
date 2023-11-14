3157cde83b134138dc8678f2ddbbb71c204a777e
happyfool553 <tosia.klimczak@gmail.com>
2023-11-14 10:24:38 +0100

    .

---
+++ projekty na ocene/kolko.krzyzyk.py
@@ -0,0 +1,28 @@
+#twozrenie zmiennych miejsc
+jeden = "1"
+dwa = "2"
+trzy = "3"
+cztery = "4"
+piec = "5"
+szesc = "6"
+siedem = "7"
+osiem = "8"
+dziewiec = "9"
+wybranaliczba = ""
+
+def powitanie():
+    print("Witaj!Zagrajmy w kolko i krzyzyk. \n ")
+    print("Zasady sa proste - wybierasz miejsce w ktorym chcesz postawic kolko i dazysz, by ustawic 3 kola pod rzad. Powodzenia!")
+
+
+
+def ruchgracza():
+    print("Wybierz miejsce, w ktorym chcesz wstawic swoj znak")
+    print("" ,jeden,"/", dwa,"/",trzy,"\n", cztery,"/", piec,"/", szesc,"\n", siedem,"/", osiem,"/", dziewiec)
+    wybranaliczba = int(input("Wpisz liczbe od 1 do 9 "))
+
+    
+
+
+
+
