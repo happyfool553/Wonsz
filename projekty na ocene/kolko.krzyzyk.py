f658c5233e28457b1031c929f5e658790c08b206
happyfool553 <tosia.klimczak@gmail.com>
2023-11-28 09:27:01 +0100

    sa jeszcze bledy do naprawienia: program nie zatrzymuje sie, nawet gdy ktos wygra; bot nigdy nie wygrywa bo jest za glupi

--- projekty na ocene/kolko.krzyzyk.py
+++ projekty na ocene/kolko.krzyzyk.py
@@ -1,9 +1,12 @@
 import random
 import time
-
+#            bledy do naprawienia: nie konczy grym gdy ktos wygrywa; bardzo latwo wygrac z nim
 #twozrenie zmiennych miejsc
 lista = [1,2,3,4,5,6,7,8,9]
 wybranaliczba = ""
+w_gracza = 0
+w_bota= 0
+numerwliscie = 10
 
 def powitanie():
     print("Witaj!Zagrajmy w kolko i krzyzyk. \n ")
@@ -14,22 +17,26 @@ def powitanie():
 def ruchgracza():    
     print("Teraz twoja runda. Wybierz miejsce, w ktorym chcesz wstawic swoj znak")
     print("" ,lista[0],"/", lista[1],"/",lista[2],"\n", lista[3],"/", lista[4],"/", lista[5],"\n", lista[6],"/", lista[7],"/", lista[8])
-    wybranaliczba = int(input("Wybierz liczbe od 1 do 9 "))
-      
-    numerwliscie = (wybranaliczba - 1)
-    lista[numerwliscie] = "o"
-    print("" ,lista[0],"/", lista[1],"/",lista[2],"\n", lista[3],"/", lista[4],"/", lista[5],"\n", lista[6],"/", lista[7],"/", lista[8])
+    def wybieranieruchu():
+        wybranaliczba = int(input("Wybierz liczbe od 1 do 9 "))
+        if lista[wybranaliczba]=="o" or lista[wybranaliczba]=="x":
+            wybieranieruchu()
+        else: 
+            numerwliscie = (wybranaliczba - 1)
+            lista[numerwliscie] = "o"
+            print("" ,lista[0],"/", lista[1],"/",lista[2],"\n", lista[3],"/", lista[4],"/", lista[5],"\n", lista[6],"/", lista[7],"/", lista[8])
+            time.sleep(3)
+    wybieranieruchu()        
    
 def ruchbota():
     print("Teraz moja runda!")
     time.sleep(1)
     def losowanieruchu():
         losbota = random.randint(0,8)
-        if losbota=="o" or losbota=="x":
+        if lista[losbota]=="o" or lista[losbota]=="x":
             losowanieruchu()
         else: 
-            ruchbota=(losbota-1)
-            lista[ruchbota] = "x"
+            lista[losbota] = "x"
             print("" ,lista[0],"/", lista[1],"/",lista[2],"\n", lista[3],"/", lista[4],"/", lista[5],"\n", lista[6],"/", lista[7],"/", lista[8])
             time.sleep(3)
     losowanieruchu()        
@@ -39,95 +46,138 @@ def sprawdanie_wygranej_gracza():
 
         if lista[0]=="o":
             print("Wygales!")
+            w_gracza = 1
 
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[3]==lista[4]==lista[5]:
         if lista[3]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[6]==lista[7]==lista[8]:
         if lista[6]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[0]==lista[4]==lista[8]:
         if lista[0]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[2]==lista[4]==lista[6]:
         if lista[2]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[0]==lista[3]==lista[6]:
         if lista[0]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[1]==lista[4]==lista[7]:
         if lista[1]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[2]==lista[5]==lista[8]:
         if lista[2]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
 def sprawdanie_wygranej_bota():
     if lista[0]==lista[1]==lista[2]:
 
         if lista[0]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[3]==lista[4]==lista[5]:
         if lista[3]=="o":
             print("Wygales")
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[6]==lista[7]==lista[8]:
         if lista[6]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[0]==lista[4]==lista[8]:
         if lista[0]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[2]==lista[4]==lista[6]:
         if lista[2]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[0]==lista[3]==lista[6]:
         if lista[0]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     if lista[1]==lista[4]==lista[7]:
         if lista[1]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
-            print("Udalo mi sie wygrac!")   
+            print("Udalo mi sie wygrac!")  
+            w_bota= 1 
     if lista[2]==lista[5]==lista[8]:
         if lista[2]=="o":
             print("Wygales")
+            w_gracza = 1
         else:
             print("Udalo mi sie wygrac!")
+            w_bota= 1
     else: jedna_runda()
+
+def zakonczenie():
+    print("Dziekuje za gre, jesli chcesz zagrac jeszcze raz, uruchom pogram ponownie!")    
 def jedna_runda():
     ruchgracza()
     sprawdanie_wygranej_gracza()
+    if w_gracza==1:{
+        zakonczenie()
+    }
+    elif w_bota==1:{zakonczenie()}
     ruchbota()
     sprawdanie_wygranej_bota()
+    if w_bota==1:{
+        zakonczenie()
+    }
+    elif w_bota==1:{zakonczenie()}    
 powitanie()
 time.sleep(3)
 jedna_runda()
+
 
 
 
