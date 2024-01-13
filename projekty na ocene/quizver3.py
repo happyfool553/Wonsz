445e0308c654f3fd45b0faf3d8d5c70be210aaf4
happyfool553 <150125893+happyfool553@users.noreply.github.com>
2024-01-13 16:07:20 +0000

    .

--- projekty na ocene/quizver3.py
+++ projekty na ocene/quizver3.py
@@ -1,7 +1,6 @@
-
-odp_gr = " "
+odp_gr=""
 nr_pytania=1
-print("Witaj Przed toba quiz, ktory ma na calu zaproponowac ci piosenke zespolu Tomorrow X Together, na podstawie twoich preferencji. \nPrzed toba kilkanascie pytan - po kazdej proponowanej odpowiedzi to, co masz wpisac do konsoli bedzie zapisane w ten sposob (odpowiedz). \nMilego quizowania!")
+print("\nWitaj! Przed toba quiz, ktory ma na celu zaproponowanie ci piosenke zespolu Tomorrow X Together \nna podstawie twoich preferencji. \nPrzed toba kilkanascie pytan - po kazdej proponowanej odpowiedzi to, co masz wpisac do konsoli \nbedzie zapisane w ten sposob: (odpowiedz). \nJesli wpiszesz cos innego, niz powinienes - nie martw sie! Pytanie wyswietli sie jeszcze raz, \na ty bedziesz mogl spokojnie odpowiedziec na pytanie.\nMilego quizowania!\n")
 #tworzenie slownika
 piosenki={
     "CROWN": 0,
@@ -28,123 +27,132 @@ ctf= 0
 LEveL= 0
 
 print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
+# 11111111111111111111111111111111111111111111111
+while odp_gr !="w"or odp_gr !="bbv"or odp_gr !="mp":
+    odp_gr= input("1. Lubisz wesole, pozytywnie brzmiace piosenki (w), te o mniej pozytywnym nastroju (mp) \nczy moze dajacy  *bad boy/girl* vibe (bbv)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="w"or odp_gr =="bbv"or odp_gr =="mp":
+        break
+if odp_gr == "w" :
+    CROWN= CROWN+1
+    blue_hour= blue_hour+1
+    lovesong =lovesong+1
+    magic= magic+1
+    srr= srr+1
+    ctf= ctf+1
+    LEveL= LEveL+1
+elif   odp_gr == "bbv":   
+    gbgb= gbgb+1   
+elif odp_gr == "mp":
+    run_away= run_away+1
+    cant_you_see_me=cant_you_see_me+1
+else:
+    print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
+print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
+#2222222222222222222222222222222222222222222
+while odp_gr !="sm"or odp_gr !="sp"or odp_gr !="o" or odp_gr!="c":
+    odp_gr= input("2.Ktora z podanych czynnosci lubisz najbardziej? Sluchanie muzyki(sm), spanie(sp), \nogladanie seriali lub fimow(o), czy czytanie(c)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="sm"or odp_gr =="sp"or odp_gr =="o"or odp_gr =="c":
+        break
+if odp_gr=="sm":
+    magic =magic+1
+    srr=srr+1
+elif odp_gr=="sp":
+    cant_you_see_me=cant_you_see_me+1
+elif odp_gr=="o":
+    gbgb=gbgb+1
+    ctf=ctf+1
+    LEveL=LEveL+1
+elif odp_gr=="c":
+    lovesong=lovesong+1
+    CROWN=CROWN+1
+    run_away=run_away+1
+    blue_hour=blue_hour+1
+else:
+    print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
+print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
-def pytanie1():
-    odp_gr= input("1. Czy lubisz wesole, pozytywnie brzmiace piosenki (w), te o mniej pozytywnym nastroju (mp) czy moze dajacy *bad boy/girl* vibe (bbv)?")
-def ptk1():
-    if odp_gr == "w" :
-        CROWN= CROWN+1
-        blue_hour= blue_hour+1
-        lovesong =lovesong+1
-        magic= magic+1
-        srr= srr+1
-        ctf= ctf+1
-        LEveL= LEveL+1
-    elif   odp_gr == "bbv":   
-        gbgb= gbgb+1   
-    elif odp_gr == "mp":
-        run_away= run_away+1
-        cant_you_see_me=cant_you_see_me+1
-    else:
-        print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
-    print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
+#33333333333333333333333333333333333333
+while odp_gr !="f"or odp_gr !="b"or odp_gr !="g" or odp_gr !="zi" or odp_gr !="zo" or odp_gr !="cz" or odp_gr !="r":
+    odp_gr= input("3.Jaki jest twoj ulubiony kolor z podanych? Fioletowy(f),  blekitny(b), granatowy(g), \nzielony(zi), zolty (zo), czerwony(cz), czy rozowy(r)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="f"or odp_gr =="b"or odp_gr =="g" or odp_gr =="zi" or odp_gr =="zo" or odp_gr =="cz" or odp_gr =="r":
+        break
+if odp_gr=="f":
+    cant_you_see_me=cant_you_see_me+1
+elif odp_gr=="b":
+    blue_hour=blue_hour+1
+    magic=magic+1
+elif odp_gr=="g":
+    lovesong=lovesong+1
+    ctf=ctf+1
+elif odp_gr=="zi":
+    run_away=run_away+1
+    LEveL=LEveL+1
+elif odp_gr=="zo":
+    CROWN=CROWN=+1
+elif odp_gr=="cz":
+    gbgb=gbgb+1
+elif odp_gr=="r":
+    srr=srr+1
+else:
+    print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
+print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
-def pytanie2():
-    odp_gr= input("2.Ktora z podanych czynnosci lubisz najbardziej? sluchanie muzyki(sm), spanie(sp), ogladanie seriali lub fimow(o), czytanie(c)")
-#text.lower(odp_gr)
-def ptk2():
-    if odp_gr=="sm":
-        magic =magic+1
-        srr=srr+1
-    elif odp_gr=="sp":
-        cant_you_see_me=cant_you_see_me+1
-    elif odp_gr=="o":
-        gbgb=gbgb+1
-        ctf=ctf+1
-        LEveL=LEveL+1
-    elif odp_gr=="c":
-        lovesong=lovesong+1
-        CROWN=CROWN+1
-        run_away=run_away+1
-        blue_hour=blue_hour+1
-    else:
-        print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
-    print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
+#444444444444444444444444444
+while odp_gr !="w"or odp_gr !="l"or odp_gr !="j" or odp_gr!="z":
+    odp_gr= input("4. Ktora z por roku jest twoja ulubiona? Wiosna(w), lato(l), jesien(j), czy zima(z)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="w"or odp_gr =="l"or odp_gr =="j" or odp_gr=="z":
+        break
+if odp_gr=="w":
+    blue_hour=blue_hour+1
+elif odp_gr=="l":
+    CROWN=CROWN+1
+    magic=magic+1
+    srr=srr+1
+elif odp_gr=="j":
+    run_away=run_away+1
+    cant_you_see_me=cant_you_see_me+1
+    gbgb=gbgb+1
+    LEveL=LEveL+1
+elif odp_gr=="z":
+    lovesong=lovesong+1
+    ctf=ctf+1
+else:
+    print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
+print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
-def pytanie3(): 
-    odp_gr= input("3.Jaki jest twoj ulubiony kolor z podanych? Fioletowy(f),  blekitny(b), granatowy(g), zielony(zi), zolty (zo), czerwony(cz), rozowy(r)")
-def ptk3():
-    if odp_gr=="f":
-        cant_you_see_me=cant_you_see_me+1
-    elif odp_gr=="b":
-        blue_hour=blue_hour+1
-        magic=magic+1
-    elif odp_gr=="g":
-        lovesong=lovesong+1
-        ctf=ctf+1
-    elif odp_gr=="zi":
-        run_away=run_away+1
-        LEveL=LEveL+1
-    elif odp_gr=="zo":
-        CROWN=CROWN=+1
-    elif odp_gr=="cz":
-        gbgb=gbgb+1
-    elif odp_gr=="r":
-        srr=srr+1
-    else:
-        print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
-    print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
-
-def pytanie4():
-    odp_gr= input("4. Ktora z por roku jest twoja ulubiona? Wiosna(w), lato(l), jesien(j), czy zima(z)?")
-def ptk4():
-    if odp_gr=="w":
-        blue_hour=blue_hour+1
-    elif odp_gr=="l":
-        CROWN=CROWN+1
-        magic=magic+1
-        srr=srr+1
-    elif odp_gr=="j":
-        run_away=run_away+1
-        cant_you_see_me=cant_you_see_me+1
-        gbgb=gbgb+1
-        LEveL=LEveL+1
-    elif odp_gr=="z":
-        lovesong=lovesong+1
-        ctf=ctf+1
-    else:
-        print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
-    print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
-
-def pytanie5():
-    odp_gr= input("5. Najbardziej wolisz, gdy jest slonecznie(s), pada deszcz(pd), pochmurnie(c), pada snieg(ps), czy wieje(w)")
-def ptk5():
-    if odp_gr=="s":
-        CROWN=CROWN+1
-        blue_hour=blue_hour+1
-        magic=magic+1
-        srr=srr+1
-    elif odp_gr=="pd":
-        run_away=run_away+1
-        lovesong+lovesong+1
-    elif odp_gr=="c":
-        cant_you_see_me=cant_you_see_me+1
-        gbgb=gbgb+1
-    elif odp_gr=="ps":
-        blue_hour=blue_hour+1
-        ctf=ctf+1
-        LEveL=LEveL+1
-    elif odp_gr=="w":
-        cant_you_see_me=cant_you_see_me+1
-        LEveL=LEveL+1
-        gbgb=gbgb+1
-    else:
-        print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
-    print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
-
-
-
+#5555555555555555555555555555
+while odp_gr !="s"or odp_gr !="pd"or odp_gr !="c" or odp_gr=="ps" or odp_gr=="w":
+    odp_gr= input("5. Najbardziej wolisz, gdy jest slonecznie(s), pada deszcz(pd), pochmurnie(c), \npada snieg(ps), czy wieje(w)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="s"or odp_gr =="pd"or odp_gr =="c" or odp_gr =="ps" or odp_gr =="w":
+        break
+if odp_gr=="s":
+    CROWN=CROWN+1
+    blue_hour=blue_hour+1
+    magic=magic+1
+    srr=srr+1
+elif odp_gr=="pd":
+    run_away=run_away+1
+    lovesong+lovesong+1
+elif odp_gr=="c":
+    cant_you_see_me=cant_you_see_me+1
+    gbgb=gbgb+1
+elif odp_gr=="ps":
+    blue_hour=blue_hour+1
+    ctf=ctf+1
+    LEveL=LEveL+1
+elif odp_gr=="w":
+    cant_you_see_me=cant_you_see_me+1
+    LEveL=LEveL+1
+    gbgb=gbgb+1
+else:
+    print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
+print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 #print(piosenki.keys())
 #print(piosenki.values())
 #print("Piosenki"+ int(CROWN)+int(run_away)+int(cant_you_see_me)+int(blue_hour)+int(lovesong)+int(magic)+int(gbgb)+int(srr)+int(ctf)+int(LEveL))
--- projekty na ocene/quizver4.py
+++ projekty na ocene/quizver4.py
@@ -1,7 +1,6 @@
-
-odp_gr = " "
-
-print("Witaj Przed toba quiz, ktory ma na calu zaproponowac ci piosenke zespolu Tomorrow X Together, na podstawie twoich preferencji. \nPrzed toba kilkanascie pytan - po kazdej proponowanej odpowiedzi to, co masz wpisac do konsoli bedzie zapisane w ten sposob (odpowiedz). \nMilego quizowania!")
+odp_gr=""
+nr_pytania=1
+print("\nWitaj! Przed toba quiz, ktory ma na celu zaproponowanie ci piosenke zespolu Tomorrow X Together \nna podstawie twoich preferencji. \nPrzed toba kilkanascie pytan - po kazdej proponowanej odpowiedzi to, co masz wpisac do konsoli \nbedzie zapisane w ten sposob: (odpowiedz). \nJesli wpiszesz cos innego, niz powinienes - nie martw sie! Pytanie wyswietli sie jeszcze raz, \na ty bedziesz mogl spokojnie odpowiedziec na pytanie.\nMilego quizowania!\n")
 #tworzenie slownika
 piosenki={
     "CROWN": 0,
@@ -28,10 +27,12 @@ ctf= 0
 LEveL= 0
 
 print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
-
-#print("1. Twoj ULUBIONY nastroj piosenek")
-odp_gr= input("1. Czy lubisz wesole, pozytywnie brzmiace piosenki (w), te o mniej pozytywnym nastroju (mp) czy moze dajacy *bad boy/girl* vibe (bbv)?")
-#
+# 11111111111111111111111111111111111111111111111
+while odp_gr !="w"or odp_gr !="bbv"or odp_gr !="mp":
+    odp_gr= input("1. Lubisz wesole, pozytywnie brzmiace piosenki (w), te o mniej pozytywnym nastroju (mp) \nczy moze dajacy  *bad boy/girl* vibe (bbv)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="w"or odp_gr =="bbv"or odp_gr =="mp":
+        break
 if odp_gr == "w" :
     CROWN= CROWN+1
     blue_hour= blue_hour+1
@@ -49,8 +50,12 @@ else:
     print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
 print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
-odp_gr= input("2.Ktora z podanych czynnosci lubisz najbardziej? sluchanie muzyki(sm), spanie(sp), ogladanie seriali lub fimow(o), czytanie(c)")
-#text.lower(odp_gr)
+#2222222222222222222222222222222222222222222
+while odp_gr !="sm"or odp_gr !="sp"or odp_gr !="o" or odp_gr!="c":
+    odp_gr= input("2.Ktora z podanych czynnosci lubisz najbardziej? Sluchanie muzyki(sm), spanie(sp), \nogladanie seriali lub fimow(o), czy czytanie(c)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="sm"or odp_gr =="sp"or odp_gr =="o"or odp_gr =="c":
+        break
 if odp_gr=="sm":
     magic =magic+1
     srr=srr+1
@@ -69,7 +74,12 @@ else:
     print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
 print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
-odp_gr= input("3.Jaki jest twoj ulubiony kolor z podanych? Fioletowy(f),  blekitny(b), granatowy(g), zielony(zi), zolty (zo), czerwony(cz), rozowy(r)")
+#33333333333333333333333333333333333333
+while odp_gr !="f"or odp_gr !="b"or odp_gr !="g" or odp_gr !="zi" or odp_gr !="zo" or odp_gr !="cz" or odp_gr !="r":
+    odp_gr= input("3.Jaki jest twoj ulubiony kolor z podanych? Fioletowy(f),  blekitny(b), granatowy(g), \nzielony(zi), zolty (zo), czerwony(cz), czy rozowy(r)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="f"or odp_gr =="b"or odp_gr =="g" or odp_gr =="zi" or odp_gr =="zo" or odp_gr =="cz" or odp_gr =="r":
+        break
 if odp_gr=="f":
     cant_you_see_me=cant_you_see_me+1
 elif odp_gr=="b":
@@ -91,7 +101,12 @@ else:
     print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
 print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
-odp_gr= input("4. Ktora z por roku jest twoja ulubiona? Wiosna(w), lato(l), jesien(j), czy zima(z)?")
+#444444444444444444444444444
+while odp_gr !="w"or odp_gr !="l"or odp_gr !="j" or odp_gr!="z":
+    odp_gr= input("4. Ktora z por roku jest twoja ulubiona? Wiosna(w), lato(l), jesien(j), czy zima(z)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="w"or odp_gr =="l"or odp_gr =="j" or odp_gr=="z":
+        break
 if odp_gr=="w":
     blue_hour=blue_hour+1
 elif odp_gr=="l":
@@ -110,7 +125,12 @@ else:
     print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
 print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
-odp_gr= input("5. Najbardziej wolisz, gdy jest slonecznie(s), pada deszcz(pd), pochmurnie(c), pada snieg(ps), czy wieje(w)")
+#5555555555555555555555555555
+while odp_gr !="s"or odp_gr !="pd"or odp_gr !="c" or odp_gr=="ps" or odp_gr=="w":
+    odp_gr= input("5. Najbardziej wolisz, gdy jest slonecznie(s), pada deszcz(pd), pochmurnie(c), \npada snieg(ps), czy wieje(w)?\n")
+    odp_gr=odp_gr.lower()
+    if odp_gr =="s"or odp_gr =="pd"or odp_gr =="c" or odp_gr =="ps" or odp_gr =="w":
+        break
 if odp_gr=="s":
     CROWN=CROWN+1
     blue_hour=blue_hour+1
@@ -134,12 +154,23 @@ else:
     print("Wyglada na to, ze wpisalxs cos innego, bylo napisane w poleceniu. Twoja odpowiedz z tego pytania nie bedzie brana pod uwage w punktacji!")
 print(CROWN, run_away,cant_you_see_me,blue_hour,lovesong,magic,gbgb,srr,ctf,LEveL)
 
+piosenki={
+    "CROWN": CROWN,
+    "9 and Three Quarters (Run Away)":  run_away,
+    "Can't You See Me?":cant_you_see_me,
+    "Blue Hour": blue_hour,
+    "0X1=Lovesong (I Know I love You)": lovesong,
+    "Magic": magic,
+    "Good Boy Gone Bad": gbgb,
+    "Sugar Rush Ride": srr,
+    "Chasing that feeling": ctf,
+    "LEveL": LEveL
+}
+print(piosenki.keys())
+print(piosenki.values())
 
-
-
-
-#print(piosenki.keys())
-#print(piosenki.values())
+#if CROWN== 5: #or run_away == 5 or cant_you_see_me==5 or blue_hour==5 or magic==5 or gbgb==5 or srr==5 or ctf==5 or LEveL==5:
+    
 #print("Piosenki"+ int(CROWN)+int(run_away)+int(cant_you_see_me)+int(blue_hour)+int(lovesong)+int(magic)+int(gbgb)+int(srr)+int(ctf)+int(LEveL))
 
 
